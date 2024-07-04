39a8a06a2e8bc889a858436cb427c0c09c3aaf4d
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-04 11:10:30 +0300

    feat: custom button component

---
+++ youtube/src/app/shared/custom-button/custom-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrl: './custom-button.component.scss',
+  standalone: true,
+})
+export default class CustomButtonComponent {}
