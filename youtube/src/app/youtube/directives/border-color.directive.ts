21d8190f4be6a8d00ee9ac4fca7ec578f81f6fca
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-04 11:11:53 +0300

    feat: border color directive

---
+++ youtube/src/app/youtube/directives/border-color.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appBorderColor]',
+  standalone: true,
+})
+export class BorderColorDirective {
+  @Input() publishedAt = '';
+
+  @HostBinding('style.borderColor') get borderColor() {
+    return this.publishedAt ? this.getColorByDate(this.publishedAt) : '';
+  }
+
+  getColorByDate(date: string): string {
+    const now = new Date();
+    const publishedAt = new Date(date);
+    const diffInMonths = (now.getFullYear() - publishedAt.getFullYear()) * 12 + now.getMonth() - publishedAt.getMonth();
+    const diffInDays = Math.round((now.getTime() - publishedAt.getTime()) / (1000 * 60 * 60 * 24));
+
+    const colorConditions = [
+      { color: 'blue', condition: diffInDays < 7 },
+      { color: 'green', condition: diffInDays >= 7 && diffInDays <= 30 },
+      { color: 'yellow', condition: diffInMonths >= 1 && diffInMonths <= 6 },
+      { color: 'red', condition: diffInMonths > 6 },
+    ];
+
+    return colorConditions.find(({ condition }) => condition)?.color || 'blue';
+  }
+}
