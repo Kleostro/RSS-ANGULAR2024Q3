dfd304e48ad40f743b179592a6d3e05a1c325814
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-05 10:10:17 +0300

    feat: loader on the card img

--- youtube/src/app/youtube/components/video-card/video-card.component.ts
+++ youtube/src/app/youtube/components/video-card/video-card.component.ts
@@ -1,5 +1,7 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { Video } from '../../../core/header/models/video-search.model';
 import CustomButtonComponent from '../../../shared/components/custom-button/custom-button.component';
@@ -8,11 +10,17 @@ import BorderColorDirective from '../../directives/border-color.directive';
 @Component({
   selector: 'app-video-card',
   standalone: true,
-  imports: [CustomButtonComponent, MatIconModule, BorderColorDirective],
+  imports: [CustomButtonComponent, MatIconModule, BorderColorDirective, MatProgressSpinnerModule, NgClass],
   providers: [BorderColorDirective],
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.scss',
 })
 export default class VideoCardComponent {
   @Input() video!: Video;
+
+  isImageLoading = true;
+
+  imageLoaded() {
+    this.isImageLoading = false;
+  }
 }
