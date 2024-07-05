09110370a3c722494bd388909c33cfd29a2f4aaa
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-05 04:18:23 +0300

    refactor: remove app module

--- youtube/src/app/app.component.ts
+++ youtube/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import HeaderComponent from './core/header/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet],
 })
 export default class AppComponent {}
