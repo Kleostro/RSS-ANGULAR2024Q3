c452f491c4b6010baf1ea83a4cf65426791a00bd
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-07 16:03:26 +0300

    feat: 404 page

--- youtube/src/app/app.routes.ts
+++ youtube/src/app/app.routes.ts
@@ -3,8 +3,14 @@ import { Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/components/main/main.component').then((m) => m.default),
+    loadComponent: () => import('./pages/components/main/main.component').then((c) => c.default),
     title: 'youtube',
+  },
+
+  {
+    path: '**',
+    loadComponent: () => import('./core/pages/components/not-found/not-found.component').then((c) => c.default),
+    title: '404',
   },
 ];
 
---
+++ youtube/src/app/core/pages/components/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import CustomLinkComponent from '../../../../shared/components/custom-link/custom-link.component';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CustomLinkComponent],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export default class NotFoundComponent {}
