873c0ca368c65b2c027ccf25c671c35f44b967b3
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-10 23:18:20 +0300

    refactor: rewrite interceptor from a class to a function

--- youtube/src/app/app.config.ts
+++ youtube/src/app/app.config.ts
@@ -1,18 +1,17 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import routes from './app.routes';
-import youtubeInterceptorConfig from './youtube/interceptors/youtube.interceptor';
+import youtubeInterceptor from './youtube/interceptors/youtube.interceptor';
 
 const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi()),
-    youtubeInterceptorConfig,
+    provideHttpClient(withInterceptors([youtubeInterceptor])),
   ],
 };
 export default appConfig;
--- youtube/src/app/youtube/interceptors/youtube.interceptor.ts
+++ youtube/src/app/youtube/interceptors/youtube.interceptor.ts
@@ -1,18 +1,14 @@
-import { HTTP_INTERCEPTORS, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 import ENVIRONMENTS from '../../../environment/environment';
 
-class YoutubeInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<unknown>, next: HttpHandler) {
-    const modifiedRequest = req.clone({
-      setParams: { key: ENVIRONMENTS.API_KEY },
-      url: ENVIRONMENTS.API_BASE_URL + req.url,
-    });
+const youtubeInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  const modifiedReq = req.clone({
+    setParams: { key: ENVIRONMENTS.API_KEY },
+    url: ENVIRONMENTS.API_BASE_URL + req.url,
+  });
 
-    return next.handle(modifiedRequest);
-  }
-}
+  return next(modifiedReq);
+};
 
-const youtubeInterceptorConfig = { provide: HTTP_INTERCEPTORS, useClass: YoutubeInterceptor, multi: true };
-
-export default youtubeInterceptorConfig;
+export default youtubeInterceptor;
