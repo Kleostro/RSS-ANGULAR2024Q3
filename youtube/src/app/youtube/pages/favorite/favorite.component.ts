be1aa5f0fc77a4aa84669b2b0cc4635399e4948f
Max <133934232+Kleostro@users.noreply.github.com>
2024-07-21 20:51:22 +0300

    feat: implement favorite page

---
+++ youtube/src/app/youtube/pages/favorite/favorite.component.ts
@@ -0,0 +1,23 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import CustomLinkComponent from '../../../shared/components/custom-link/custom-link.component';
+import MAT_ATTRIBUTE from '../../../shared/constants/matAttribute';
+import { selectFavoriteVideos } from '../../../store/selectors/videos.selector';
+import VideoListComponent from '../../components/video-list/video-list.component';
+
+@Component({
+  selector: 'app-favorite',
+  standalone: true,
+  imports: [VideoListComponent, AsyncPipe, CustomLinkComponent],
+  templateUrl: './favorite.component.html',
+  styleUrl: './favorite.component.scss',
+})
+export default class FavoriteComponent {
+  store = inject(Store);
+
+  videos$ = this.store.select(selectFavoriteVideos);
+
+  matAttribute = MAT_ATTRIBUTE;
+}
